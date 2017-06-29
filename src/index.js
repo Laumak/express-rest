092d0f85d244afcd1866365077734f18597c9a16
Lauri Mäkinen <lauri@mits.fi>
2017-06-29 12:13:45 +0300

    Added mongoDB & mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const express = require("express")
 const bodyParser = require("body-parser")
-const morgan = require("morgan")
+const express    = require("express")
+const mongoose   = require('mongoose')
+const morgan     = require("morgan")
 
 const apiRouter = require("./routes/api")
 
 const app = express()
 const port = process.env.PORT || 4000
+
+mongoose.connect("mongodb://mongo:27017")
 
 // HTTP request logging
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
